1cd6653892787b11b65483bb91a8c9ddb5491586
Clara <10088152+claraj@users.noreply.github.com>
2025-11-04 14:35:28 -0600

    Fix apostrophe usage in favorites.py

--- favorites.py
+++ favorites.py
@@ -1,101 +1,101 @@
-"""A collaborative list of some of ITEC 2903's favorite things."""
+"""A collaborative list of some of ITEC 2903s favorite things."""
 
 # 1. Favorite Color
 
 favorite_color = ''
-print(f'One of ITEC 2903's favorite colors is {favorite_color}')
+print(f'One of ITEC 2903s favorite colors is {favorite_color}')
 
 
 # 2. Favorite Food
 
 favorite_food = ''
-print(f'One of ITEC 2903's favorite foods is {favorite_food}')
+print(f'One of ITEC 2903s favorite foods is {favorite_food}')
 
 
 # 3. Favorite Movie or TV Show
 
 favorite_movie = ''
-print(f'One of ITEC 2903's favorite movies is {favorite_movie}')
+print(f'One of ITEC 2903s favorite movies is {favorite_movie}')
 
 
 ## 4. Favorite Music
 
 favorite_music = ''
-print(f'One of ITEC 2903's favorite types of music is {favorite_music}')
+print(f'One of ITEC 2903s favorite types of music is {favorite_music}')
 
 
 ## 5. Favorite Place
 
 favorite_place = ''
-print(f'One of ITEC 2903's favorite places is {favorite_place}')
+print(f'One of ITEC 2903s favorite places is {favorite_place}')
 
 
 ## 6. Favorite Holiday
 
 favorite_holiday = ''
-print(f'One of ITEC 2903's favorite holidays is {favorite_holiday}')
+print(f'One of ITEC 2903s favorite holidays is {favorite_holiday}')
 
 ## 7. Favorite Book
 
 favorite_book = ''
-print(f'One of ITEC 2903's favorite books is {favorite_book}')
+print(f'One of ITEC 2903s favorite books is {favorite_book}')
 
 
 ## 8. Favorite Animal
 
 favorite_animal = ''
-print(f'One of ITEC 2903's favorite animals is {favorite_animal}')
+print(f'One of ITEC 2903s favorite animals is {favorite_animal}')
 
 
 ## 9. Favorite Weather
 
 favorite_weather = ''
-print(f'One of ITEC 2903's favorite type of weather is {favorite_weather}')
+print(f'One of ITEC 2903s favorite type of weather is {favorite_weather}')
 
 
 ## 10. Favorite Drink
 
 favorite_drink = ''
-print(f'One of ITEC 2903's favorite drinks is {favorite_drink}')
+print(f'One of ITEC 2903s favorite drinks is {favorite_drink}')
 
 
 ## 11. Favorite Store
 
 favorite_store = ''
-print(f'One of ITEC 2903's favorite stores is {favorite_store}')
+print(f'One of ITEC 2903s favorite stores is {favorite_store}')
 
 
 ## 12. Favorite Sport
 
 favorite_sport = ''
-print(f'One of ITEC 2903's favorite sports is {favorite_sport}')
+print(f'One of ITEC 2903s favorite sports is {favorite_sport}')
 
 
 ## 13. Favorite Programming Language
 
 favorite_programming_language = ''
-print(f'One of ITEC 2903's favorite programming languages is {favorite_programming_language}')
+print(f'One of ITEC 2903s favorite programming languages is {favorite_programming_language}')
 
 
 ## 14. Favorite Mode of Transportation
 
 favorite_transportation = ''
-print(f'One of ITEC 2903's favorite modes of transportation is {favorite_transportation}')
+print(f'One of ITEC 2903s favorite modes of transportation is {favorite_transportation}')
 
 
 ## 15. Favorite Website
 
 favorite_website = ''
-print(f'One of ITEC 2903's favorite websites is {favorite_website}')
+print(f'One of ITEC 2903s favorite websites is {favorite_website}')
 
 
 ## 16. Favorite Season
 
 favorite_season = ''
-print(f'One of ITEC 2903's favorite seasons is {favorite_season}')
+print(f'One of ITEC 2903s favorite seasons is {favorite_season}')
 
 
 ## 17. Favorite Plant, Tree or Flower
 
 favorite_plant = ''
-print(f'One of ITEC 2903's favorite plants is {favorite_plant}')
+print(f'One of ITEC 2903s favorite plants is {favorite_plant}')
